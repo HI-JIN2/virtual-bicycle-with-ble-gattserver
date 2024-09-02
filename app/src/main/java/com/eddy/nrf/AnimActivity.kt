2014a5c9e281fb197f25583373246e404e82329d
Yu Jin <qldls0307@naver.com>
2024-09-02 17:12:47 +0900

    feat: 자전거 애니메이션

--- app/src/main/java/com/eddy/nrf/AnimActivity.kt
+++ app/src/main/java/com/eddy/nrf/AnimActivity.kt
@@ -2,11 +2,8 @@ package com.eddy.nrf
 
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
-import android.os.Handler
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.eddy.nrf.databinding.ActivityAnimBinding
-
 
 class AnimActivity : AppCompatActivity() {
 
@@ -23,44 +20,11 @@ class AnimActivity : AppCompatActivity() {
         anim.start()
 
         binding.button.setOnClickListener { view ->
-            if (anim.isRunning) {
+            if (anim.isRunning)
                 anim.stop()
-            } else {
+            else
                 anim.start()
-            }
         }
 
-//        binding.buttonFast.setOnClickListener { view ->
-//            adjustAnimationSpeed(binding.imageview, anim, 25) // 각 프레임의 지속 시간을 100ms로 설정
-//        }
-//        binding.buttonSlow.setOnClickListener { view ->
-//            adjustAnimationSpeed(binding.imageview, anim, 1000) // 각 프레임의 지속 시간을 100ms로 설정
-//        }
-
-
-    }
-
-    private fun adjustAnimationSpeed(
-        imageView: ImageView,
-        animationDrawable: AnimationDrawable,
-        frameDuration: Long
-    ) {
-        val handler: Handler = Handler()
-        val frameCount = animationDrawable.numberOfFrames
-
-        val updateAnimation: Runnable = object : Runnable {
-            private var currentFrame = 0
-
-            override fun run() {
-                if (currentFrame < frameCount) {
-                    imageView.setImageDrawable(animationDrawable)
-                    animationDrawable.selectDrawable(currentFrame)
-                    currentFrame = (currentFrame + 1) % frameCount
-                    handler.postDelayed(this, frameDuration)
-                }
-            }
-        }
-
-        handler.post(updateAnimation)
     }
 }
