89db152276e91f60780cafd2c780c9e4f98beb55
Yu Jin <qldls0307@naver.com>
2024-09-03 13:03:13 +0900

    chore: 코드 개선

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,5 +49,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-//    implementation()
+    implementation(libs.tedpermission)
 }
--- app/src/main/java/com/eddy/nrf/MainActivity.kt
+++ app/src/main/java/com/eddy/nrf/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.eddy.nrf
 
-import android.Manifest
 import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.Manifest.permission.BLUETOOTH_ADVERTISE
 import android.Manifest.permission.BLUETOOTH_CONNECT
@@ -8,7 +7,6 @@ import android.Manifest.permission.BLUETOOTH_SCAN
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothGattCharacteristic
-import android.bluetooth.BluetoothGattServer
 import android.bluetooth.BluetoothGattServerCallback
 import android.bluetooth.BluetoothGattService
 import android.bluetooth.BluetoothManager
@@ -19,7 +17,6 @@ import android.bluetooth.le.AdvertiseSettings
 import android.bluetooth.le.BluetoothLeAdvertiser
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -29,16 +26,14 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.eddy.nrf.Utils.checkAllPermission
 import com.eddy.nrf.databinding.ActivityMainBinding
-
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     private lateinit var bluetoothAdapter: BluetoothAdapter
     private var bluetoothLeAdvertiser: BluetoothLeAdvertiser? = null
-
     private lateinit var enableBluetoothLauncher: ActivityResultLauncher<Intent>
 
     @RequiresApi(Build.VERSION_CODES.S)
@@ -48,174 +43,122 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //블루투스를 위한 권한을 따옵니다.
         bleInitialize()
 
-        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-        bluetoothAdapter = bluetoothManager.adapter
+        bluetoothAdapter = (getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager).adapter
+        Log.d("BluetoothAdapter", "Device name: ${bluetoothAdapter.name}")
 
-        Log.d("name", bluetoothAdapter.name.toString())
+        binding.btnStart.setOnClickListener { handleStartButtonClick() }
 
-        binding.btnStart.setOnClickListener {
-            if (bluetoothAdapter.isEnabled) {
-                startAdvertising()
-            } else {
-                // 블루투스가 꺼져있으면 켜는 과정 필요
-                if (ActivityCompat.checkSelfPermission(
-                        this,
-                        BLUETOOTH_CONNECT
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    Toast.makeText(this, "블루투스가 꺼져있어 광고를 시작할 수 없습니다.", Toast.LENGTH_SHORT).show()
-                    return@setOnClickListener
-                }
-//                bluetoothAdapter.enable() -> deprecated
-// 사용자에게 Bluetooth 활성화 요청 다이얼로그가 표시됨.
+        setupGattServer()
+    }
 
-                // 블루투스 활성화 요청
-                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                enableBluetoothLauncher.launch(enableBtIntent)
+    private fun handleStartButtonClick() {
+        if (bluetoothAdapter.isEnabled) {
+            startAdvertising()
+        } else {
+            if (!checkAllPermission(BLUETOOTH_CONNECT)) {
+                Toast.makeText(this, "블루투스가 꺼져있어 광고를 시작할 수 없습니다.", Toast.LENGTH_SHORT).show()
+                return
             }
+            requestBluetoothEnable()
         }
+    }
 
-        val gattServer: BluetoothGattServer? =
-            bluetoothManager.openGattServer(this, gattServerCallback)
-
-        // 4. GATT 서비스를 추가합니다 (서비스 및 특성 정의).
+    private fun setupGattServer() {
+        val gattServer = (getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager)
+            .openGattServer(this, gattServerCallback)
         val service = BluetoothGattService(
             Utils.HEART_RATE_UUID,
             BluetoothGattService.SERVICE_TYPE_PRIMARY
         )
-
-        // 서비스에 특성을 추가합니다.
         val characteristic = BluetoothGattCharacteristic(
             Utils.HEART_RATE_MEASUREMENT,
             BluetoothGattCharacteristic.PROPERTY_READ or BluetoothGattCharacteristic.PROPERTY_NOTIFY,
             BluetoothGattCharacteristic.PERMISSION_READ
         )
         service.addCharacteristic(characteristic)
-
-        // 서버에 서비스를 추가합니다.
         gattServer?.addService(service)
-
-
-
-
-    }
-
-    //연결에 대한 콜백
-    private val gattServerCallback = object : BluetoothGattServerCallback() {
-        override fun onConnectionStateChange(device: BluetoothDevice, status: Int, newState: Int) {
-            super.onConnectionStateChange(device, status, newState)
-            if (newState == BluetoothProfile.STATE_CONNECTED) {
-                // 클라이언트가 서버에 연결되었을 때
-                Log.d("GattServer", "Device connected: ${device.address}")
-                binding.tvDeviceInfo.text = device.address
-            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
-                // 클라이언트가 서버에서 연결이 끊어졌을 때
-                Log.d("GattServer", "Device disconnected: ${device.address}")
-                binding.tvDeviceInfo.text = "연결 실패"
-            }//address는 그냥 받아지는데 name은 왜 퍼미션 체크를 또해야지????
-        }
-
-        // 여기에서 onServiceAdded, onCharacteristicReadRequest 등 다른 콜백들도 처리할 수 있습니다.
     }
 
     private fun bleInitialize() {
-        // 런타임 권한 확인
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            if (checkSelfPermission(ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
-                checkSelfPermission(BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED ||
-                checkSelfPermission(BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED ||
-                checkSelfPermission(BLUETOOTH_ADVERTISE) != PackageManager.PERMISSION_GRANTED
-            ) {
-                requestBlePermissions()
-                return
-            }
+        if (!hasAllPermissions()) {
+            requestBlePermissions()
+        }
+    }
+
+    private fun hasAllPermissions(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            checkAllPermission(
+                ACCESS_FINE_LOCATION,
+                BLUETOOTH_SCAN,
+                BLUETOOTH_CONNECT,
+                BLUETOOTH_ADVERTISE
+            )
         } else {
-            if (checkSelfPermission(ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                requestBlePermissions()
-                return
-            }
+            checkAllPermission(ACCESS_FINE_LOCATION)
         }
     }
 
     private fun requestBlePermissions() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            requestPermissions(
-                arrayOf(
-                    ACCESS_FINE_LOCATION,
-                    BLUETOOTH_SCAN,
-                    BLUETOOTH_CONNECT,
-                    BLUETOOTH_ADVERTISE,
-                ), PERMISSION_REQUEST_CODE_S
-            )
+        val permissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            arrayOf(ACCESS_FINE_LOCATION, BLUETOOTH_SCAN, BLUETOOTH_CONNECT, BLUETOOTH_ADVERTISE)
         } else {
-            requestPermissions(
-                arrayOf(ACCESS_FINE_LOCATION),
-                PERMISSION_REQUEST_CODE
-            )
+            arrayOf(ACCESS_FINE_LOCATION)
         }
+        ActivityCompat.requestPermissions(this, permissions, getRequestPermissionCode())
+    }
+
+    private fun getRequestPermissionCode() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        PERMISSION_REQUEST_CODE_S
+    } else {
+        PERMISSION_REQUEST_CODE
     }
 
     private fun startAdvertising() {
         bluetoothLeAdvertiser = bluetoothAdapter.bluetoothLeAdvertiser
         bluetoothLeAdvertiser?.let { advertiser ->
-
-            // 광고 설정
             val settings = AdvertiseSettings.Builder()
                 .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
                 .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
-                .setConnectable(true)  // 연결 가능한 상태로 광고
+                .setConnectable(true)
                 .build()
 
-            // 광고 데이터 설정 (기기 식별을 위한 UUID 등)
             val data = AdvertiseData.Builder()
-                .setIncludeDeviceName(true)  // 기기 이름 포함
-                .addServiceUuid(Utils.HEART_RATE_P_UUID) // 예시 UUID
+                .setIncludeDeviceName(true)
+                .addServiceUuid(Utils.HEART_RATE_P_UUID)
                 .build()
 
-            Log.d("BluetoothAdvertise", data.toString())
-            Log.d("BluetoothAdvertise", data.includeDeviceName.toString())
+            Log.d("BluetoothAdvertise", "Advertise Data: $data")
 
-            // 광고 시작
-            if (ActivityCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.BLUETOOTH_ADVERTISE
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-
+            if (!checkAllPermission(BLUETOOTH_ADVERTISE)) {
                 Toast.makeText(this, "권한이 없어 광고를 시작할 수 없습니다.", Toast.LENGTH_SHORT).show()
                 requestBlePermissions()
-                advertiser.startAdvertising(settings, data, advertiseCallback)
-
                 return
             }
+
             advertiser.startAdvertising(settings, data, advertiseCallback)
         }
     }
 
-    // 광고 콜백
+    private val gattServerCallback = object : BluetoothGattServerCallback() {
+        override fun onConnectionStateChange(device: BluetoothDevice, status: Int, newState: Int) {
+            super.onConnectionStateChange(device, status, newState)
+            val message = when (newState) {
+                BluetoothProfile.STATE_CONNECTED -> "Device connected: ${device.address}"
+                BluetoothProfile.STATE_DISCONNECTED -> "Device disconnected: ${device.address}"
+                else -> "Unknown state"
+            }
+            Log.d("GattServer", message)
+            binding.tvDeviceInfo.text =
+                if (newState == BluetoothProfile.STATE_CONNECTED) device.address else "연결 실패"
+        }
+    }
+
     private val advertiseCallback = object : AdvertiseCallback() {
         override fun onStartSuccess(settingsInEffect: AdvertiseSettings?) {
             Log.d("BluetoothAdvertise", "Advertising started successfully")
-
             binding.tvStatus.text = "광고 중"
-
         }
 
         override fun onStartFailure(errorCode: Int) {
@@ -223,21 +166,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun requestBluetoothEnable() {
+        val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        enableBluetoothLauncher.launch(enableBtIntent)
+    }
+
     override fun onDestroy() {
         super.onDestroy()
-        // 광고 중단
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                BLUETOOTH_ADVERTISE
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+        if (!checkAllPermission(BLUETOOTH_ADVERTISE)) {
             Toast.makeText(this, "권한이 없어 광고를 중단할 수 없습니다.", Toast.LENGTH_SHORT).show()
             return
         }
@@ -245,13 +181,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        val ADVERTISE_SUCCESS = 0
-        val ADVERTISE_FAILED_DATA_TOO_LARGE = 1
-        val ADVERTISE_FAILED_TOO_MANY_ADVERTISERS = 2
-        val ADVERTISE_FAILED_ALREADY_STARTED = 3
-        val ADVERTISE_FAILED_INTERNAL_ERROR = 4
-        val ADVERTISE_FAILED_FEATURE_UNSUPPORTED = 5
-        private var PERMISSION_REQUEST_CODE_S = 101
-        private var PERMISSION_REQUEST_CODE = 100
+        private const val PERMISSION_REQUEST_CODE_S = 101
+        private const val PERMISSION_REQUEST_CODE = 100
     }
 }
