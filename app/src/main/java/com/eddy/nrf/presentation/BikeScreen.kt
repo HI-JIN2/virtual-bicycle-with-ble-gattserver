8012ba030bec73c4cc0d16ef45a05d3823a8f6b0
Yu Jin <qldls0307@naver.com>
2024-09-09 15:47:14 +0900

    feat: information component

--- app/src/main/java/com/eddy/nrf/presentation/BikeScreen.kt
+++ app/src/main/java/com/eddy/nrf/presentation/BikeScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -61,11 +62,35 @@ fun BikeScreen() {
         }
         Box(
             modifier = Modifier
-                .height(30.dp)  // 고정된 높이 30dp
+                .height(50.dp)  // 고정된 높이 30dp
                 .fillMaxWidth()  // 너비는 부모의 너비로 채움
                 .background(Color.White)
                 .padding(10.dp)
         ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),  // Row가 부모의 너비를 가득 채움
+                horizontalArrangement = Arrangement.spacedBy(
+                    100.dp,
+                    Alignment.CenterHorizontally
+                ),  // 요소들을 중앙에 정렬하고 16dp 간격 추가
+                verticalAlignment = Alignment.CenterVertically
+            )  // 세로 방향으로 중앙 정렬
+            {
+                Text(text = "08:30 PM", modifier = Modifier)
+                Text(text = "ODO")
+                Text(text = "190km")
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically // 세로 방향으로 중앙 정렬
+
+                ) {
+                    Text(text = "100%")
+                    Image(
+                        painter = painterResource(id = R.drawable.img_battery),  // 리소스 이미지 사용
+                        contentDescription = "Example Image",
+                    )
+                }
+            }
         }
     }
 
