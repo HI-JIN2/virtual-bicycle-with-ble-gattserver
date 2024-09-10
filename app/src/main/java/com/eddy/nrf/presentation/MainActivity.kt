3ffebf58ba2f1b1196b3d855be02ec21b0ce8029
Yu Jin <qldls0307@naver.com>
2024-09-10 13:06:25 +0900

    chore: init -> main

--- app/src/main/java/com/eddy/nrf/presentation/MainActivity.kt
+++ app/src/main/java/com/eddy/nrf/presentation/MainActivity.kt
@@ -12,13 +12,11 @@ import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
 import com.eddy.nrf.bluetooth.BluetoothServiceManager
 import com.eddy.nrf.presentation.ui.BikeScreen
 import com.eddy.nrf.presentation.ui.theme.NRFTheme
 
-class InitActivity : ComponentActivity() {
+class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     private lateinit var bluetoothServiceManager: BluetoothServiceManager
 
